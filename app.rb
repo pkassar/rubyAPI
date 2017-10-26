906cde21317cd9e91c5b5d5a95ddb155ceefcbb4
Paul Kassar <paulkassar@gmail.com>
2017-10-26 17:47:29 +0100

    need to update the class structure

--- app.rb
+++ app.rb
@@ -22,39 +22,17 @@ def average(id)
   p total_spent(id)/list_prices(id).count
 end
 
-# def most_loyal
-#   @data['data'].count { |user| user['spend'] == "75.21" }
-# end
-
-# def test
-#   @data['data'].each do |user|
-#     puts list_prices(|user|).count
-#   end
-# end
-
 def counting_transactions_by(arg)
   @data['data'].each_with_object(Hash.new(0)) { |h1, h2| h2[h1[arg]] += 1 }
 end
 
-
-
-@purchases_by_customer = @data['data'].each_with_object(Hash.new(0)) { |h1, h2| h2[h1["user_id"]] += 1 }
-
-
-
 def most_loyal
-  p @purchases_by_customer.max_by{|k,v| v}
+  p counting_transactions_by("user_id").max_by{ |k,v| v}.first
 end
-
 
 def most_sold
   p counting_transactions_by("item").max_by{ |k,v| v}.first
 end
-# most_loyal
-most_sold
-# p @data['data']
 
-# ary = [1,2,3,4,5]
-# ary.each do |i|
-#    puts i
-# end
+most_sold
+most_loyal
